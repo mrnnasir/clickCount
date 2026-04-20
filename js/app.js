bf73821ca99a820f210f25a8d65f911e65d52818
mrnnasir <raihan80@gmail.com>
2026-04-20 23:11:26 +0200

    Updated submit button

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 let score = 0;
-let time = 5;
+let time = 2;
 
 const playerName = document.getElementById('playerName'); // Input Player Name
 const btn1 = document.getElementById("btn1"); // Click me button
 const btn2 = document.getElementById("btn2"); // Submit button
 const scoreDisplay = document.getElementById("scoreDisplay")
 const timeLeft = document.getElementById("timeLeft");
-// const result = document.getElementById("result");
+
 
 let timerStarted = false;
 
@@ -51,12 +51,49 @@ function enableSubmitButton() {
   btn2.disabled = false;
 }
 
-btn2.addEventListener("click", submitButton, { once: true });
+btn2.addEventListener("click", submitButton);
 
-function submitButton() {
+async function submitButton() {
+  let removeEl = document.getElementsByClassName('message');
+  [...removeEl].forEach(el => el.remove());
 
-  let resultP = document.createElement("p");
-  resultP.classList.add("result");
-  resultP.innerHTML = 'Player name: '+ playerName.value + ' and the score is: ' + score;
-  document.body.appendChild(resultP);
-} //TODO: Need to add score
+  const name = playerName.value.trim();
+  if (!name) { // If no name is given
+    showMessage("Please enter valid name");
+    return;
+  }
+
+  try {
+    const response = await fetch("https://hooks.zapier.com/hooks/catch/8338993/ujs9jj9/", {
+      method: "POST",
+      body: JSON.stringify({ // .stringify converts to String
+        name: name,
+        score: score,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    await response.json(); // Stores actual usable JSON data to data variable
+    showMessage("Score saved successfully");
+    btn2.disabled = true;
+
+    let resultP = document.createElement("p");
+    resultP.classList.add("result");
+    resultP.innerHTML = 'Player name: '+ playerName.value + ' and the score is: ' + score;
+    document.body.appendChild(resultP);
+  }
+
+  catch (error) {
+    showMessage("Failed to save score");
+  }
+}
+
+function showMessage(msg) {
+  let messageP = document.createElement("p");
+  messageP.classList.add("message");
+  messageP.innerHTML = msg;
+  document.body.appendChild(messageP);
+}
